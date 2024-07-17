1acb1ac2bf5a0dec360df5341ab3f1b0d6d13796
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-07-17 22:56:56 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ class MainProgram {
   private name = "dtr";
   private description =
     "DTR is an open source developer helping CLI tool build on typescript";
-  private version = "0.2.4";
+  private version = "0.2.5";
   private program: Command;
 
   constructor() {
