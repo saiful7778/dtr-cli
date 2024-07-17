a8475e7e7643f6aa8aa920eee0f6ac36d2252ab8
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-07-17 16:24:06 +0600

    update __dirname to es6+

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,6 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export const rootUrl = __dirname;
+export const rootUrl = path.dirname(fileURLToPath(import.meta.url));
 
 export const globalConfigPath = path.join(rootUrl, "../configData.json");
